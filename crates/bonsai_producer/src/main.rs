f914bfebd34218bfbc0a5506c606f47f7fe773dd
George Duffett <gduffett@financialforce.com>
2023-08-16 21:09:16 +0100

    feat: subscribe

--- crates/bonsai_producer/src/main.rs
+++ crates/bonsai_producer/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use redis;
+use redis::{Client, RedisResult};
+
+fn main() -> RedisResult<()> {
+    let client = Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    let mut pubsub = con.as_pubsub();
+    pubsub.subscribe("channel_1")?;
+    pubsub.subscribe("channel_2")?;
+
+    loop {
+        let msg = pubsub.get_message()?;
+        let payload: String = msg.get_payload()?;
+        println!("channel '{}': {}", msg.get_channel_name(), payload);
+    }
 }
