1123275050d34752fcf072fa23b0ff4c2c8eb143
george <george.duffett@icloud.com>
2023-08-20 13:45:43 +0100

    feat: shared state across threads

--- crates/bonsai_producer/src/main.rs
+++ crates/bonsai_producer/src/main.rs
@@ -6,12 +6,12 @@ use std::sync::Mutex;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
-async fn bonsai_router(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn bonsai_router(req: Request<Body>, count: i32) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/foo") => {
-            *response.body_mut() = Body::from("FOO");
+            *response.body_mut() = Body::from(format!("FOO {}", count));
         }
         (&Method::GET, "/bar") => {
             *response.body_mut() = Body::from("BAR");
@@ -34,14 +34,15 @@ async fn main() {
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(move |_conn| {
-        let counter = counter.clone();
-        // service_fn converts our function into a `Service`
-        async move {
-            let mut counter = *counter.lock().unwrap();
-            counter += 1;
-            println!("{:}", counter);
-            Ok::<_, Infallible>(service_fn(|req: Request<Body>| async {
-                bonsai_router(req).await
+        let counter = Arc::clone(&counter);
+        async {
+            // service_fn converts our function into a `Service`
+            Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                let mut count = counter.lock().unwrap();
+                *count += 1;
+                println!("{:}", count);
+                let count_copy = *count;
+                async move { bonsai_router(req, count_copy).await }
             }))
         }
     });
