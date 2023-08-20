6839445357e3a8e2aeafcdc1745262ac965bb46e
george <george.duffett@icloud.com>
2023-08-20 14:51:58 +0100

    feat: share route table with mutex

--- crates/bonsai_producer/src/main.rs
+++ crates/bonsai_producer/src/main.rs
@@ -1,21 +1,18 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
 
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server, StatusCode};
 
 async fn bonsai_router(
     req: Request<Body>,
+    route_table: Arc<Mutex<HashMap::<&str, String>>>,
 ) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
 
-    let mut route_table = HashMap::<&str, String>::new();
-    route_table.insert("/foo", String::from("foo"));
-    route_table.insert("/far", String::from("far"));
-    route_table.insert("/bar", String::from("bar"));
-
-    match route_table.get(req.uri().path()) {
+    match route_table.lock().unwrap().get(req.uri().path()) {
         Some(x) => {
             *response.body_mut() = Body::from(x.clone());
         }
@@ -32,19 +29,26 @@ async fn main() {
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
+    let route_table = Arc::new(Mutex::new(HashMap::<&str, String>::new()));
+    let route_table_ref = Arc::clone(&route_table);
 
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(move |_conn| {
+        let route_table_ref = Arc::clone(&route_table_ref);
         async {
             // service_fn converts our function into a `Service`
             Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
-                async { bonsai_router(req).await }
+            let route_table_ref = Arc::clone(&route_table_ref);
+                async { bonsai_router(req, route_table_ref).await }
             }))
         }
     });
 
     let server = Server::bind(&addr).serve(make_svc);
+
+    route_table.lock().unwrap().insert("/foo", String::from("foo"));
+    route_table.lock().unwrap().insert("/bar", String::from("bar"));
 
     // Run this server for... forever!
     if let Err(e) = server.await {
