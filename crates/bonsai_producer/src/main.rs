c57c34823074db0d3ce9e5d08443c0449fa93b94
george <george.duffett@icloud.com>
2023-08-20 13:24:21 +0100

    feat: async server

--- crates/bonsai_producer/src/main.rs
+++ crates/bonsai_producer/src/main.rs
@@ -1,22 +1,24 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Method, Body, Request, Response, Server, StatusCode};
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 async fn bonsai_router(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/foo") => {
             *response.body_mut() = Body::from("FOO");
-        },
+        }
         (&Method::GET, "/bar") => {
             *response.body_mut() = Body::from("BAR");
-        },
+        }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-        },
+        }
     };
 
     Ok(response)
@@ -27,11 +29,21 @@ async fn main() {
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
+    let counter = Arc::new(Mutex::new(0));
+
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
+    let make_svc = make_service_fn(move |_conn| {
+        let counter = counter.clone();
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(bonsai_router))
+        async move {
+            let mut counter = *counter.lock().unwrap();
+            counter += 1;
+            println!("{:}", counter);
+            Ok::<_, Infallible>(service_fn(|req: Request<Body>| async {
+                bonsai_router(req).await
+            }))
+        }
     });
 
     let server = Server::bind(&addr).serve(make_svc);
