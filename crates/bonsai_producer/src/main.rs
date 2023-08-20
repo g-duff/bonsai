4a5a3302e5dc8d68a88182171afde730355b9342
george <george.duffett@icloud.com>
2023-08-20 14:42:54 +0100

    feat: hashmap route table wip

--- crates/bonsai_producer/src/main.rs
+++ crates/bonsai_producer/src/main.rs
@@ -1,22 +1,25 @@
+use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::sync::Arc;
-use std::sync::Mutex;
 
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Request, Response, Server, StatusCode};
 
-async fn bonsai_router(req: Request<Body>, count: i32) -> Result<Response<Body>, Infallible> {
+async fn bonsai_router(
+    req: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
 
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/foo") => {
-            *response.body_mut() = Body::from(format!("FOO {}", count));
+    let mut route_table = HashMap::<&str, String>::new();
+    route_table.insert("/foo", String::from("foo"));
+    route_table.insert("/far", String::from("far"));
+    route_table.insert("/bar", String::from("bar"));
+
+    match route_table.get(req.uri().path()) {
+        Some(x) => {
+            *response.body_mut() = Body::from(x.clone());
         }
-        (&Method::GET, "/bar") => {
-            *response.body_mut() = Body::from("BAR");
-        }
-        _ => {
+        None => {
             *response.status_mut() = StatusCode::NOT_FOUND;
         }
     };
@@ -29,20 +32,14 @@ async fn main() {
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let counter = Arc::new(Mutex::new(0));
 
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(move |_conn| {
-        let counter = Arc::clone(&counter);
         async {
             // service_fn converts our function into a `Service`
             Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
-                let mut count = counter.lock().unwrap();
-                *count += 1;
-                println!("{:}", count);
-                let count_copy = *count;
-                async move { bonsai_router(req, count_copy).await }
+                async { bonsai_router(req).await }
             }))
         }
     });
